fb94154d70267b85c2189ded12804a804040fc1b
habibamagdyy <115741344+habibamagdyy@users.noreply.github.com>
2023-12-12 21:48:09 +0200

    Update Maxsequences.java

--- taskk2/Maxsequences.java
+++ taskk2/Maxsequences.java
@@ -2,29 +2,27 @@
 package maxsequences;
 import java.util.Scanner;
 public class Maxsequences {
-    public static void main(String[] args) {
+ 
+public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         String S = scanner.next();
-        int maxSubsequenceSize = calculateMaxSubsequenceSize(S);
-        System.out.println(maxSubsequenceSize);
+        int maxSize = calculateMaxSubsequenceSize(N, S);
+        System.out.println(maxSize);
+
         scanner.close();
     }
 
-    private static int calculateMaxSubsequenceSize(String str) {
-        int maxSubsequenceSize = 1;
-        int currentSize = 1;
-        for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) != str.charAt(i - 1)) {
-                currentSize++;
-            } else {
-                maxSubsequenceSize = Math.max(maxSubsequenceSize, currentSize);
-                currentSize = 1;
+    private static int calculateMaxSubsequenceSize(int N, String S) {
+        int maxSize = 1; 
+
+        for (int i = 1; i < N; i++) {
+            if (S.charAt(i) != S.charAt(i - 1)) {
+                maxSize++;
             }
         }
-        maxSubsequenceSize = Math.max(maxSubsequenceSize, currentSize);
 
-        return maxSubsequenceSize;
+        return maxSize;
     }
-    
 }
+
