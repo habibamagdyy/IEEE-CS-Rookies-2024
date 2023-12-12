26384917d923e7d02b93640f62adbfd9a51a7237
habibamagdyy <115741344+habibamagdyy@users.noreply.github.com>
2023-12-12 21:05:13 +0200

    Update Task4.java

--- Task_1/checkcode/task4/Task4.java
+++ Task_1/checkcode/task4/Task4.java
@@ -3,34 +3,35 @@ package task4;
 import java.util.Scanner;
 
 public class Task4 {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int A = scanner.nextInt(); 
+public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+ 
+        // Read input
+        int A = scanner.nextInt();
         int B = scanner.nextInt();
-
-        int length = A + B + 1;
-
-        String code = scanner.next(); 
-        if (code.charAt(A) == '-' && code.length() == length) {
-            boolean isValid = true;
-            for (int i = 0; i < length; i++) {
-                if (i != A && !Character.isDigit(code.charAt(i))) {
-                    isValid = false;
-                    break;
-                }
-            }
-            if (isValid) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
-            }
-        } else {
-            System.out.println("No");
-        }
+        String S = scanner.next();
+    
+        boolean isValid = isValidCode(A, B, S);
+        System.out.println(isValid ? "Yes" : "No");
 
         scanner.close();
+    }
+ 
+    private static boolean isValidCode(int A, int B, String S) {
+
+        if (S.length() != A + B + 1) {
+            return false;
+        }
+        if (S.charAt(A) != '-') {
+            return false;
+        }
+        for (int i = 0; i < A + B + 1; i++) {
+            if (i != A && !Character.isDigit(S.charAt(i))) {
+                return false;
+            }
+        }
+ 
+        return true;
     }
 }
     
