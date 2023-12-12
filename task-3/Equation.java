fc68a0a566636d1733a18f57facac2e527ee06e8
habibamagdyy <115741344+habibamagdyy@users.noreply.github.com>
2023-12-12 22:21:26 +0200

    Update Equation.java

--- task-3/Equation.java
+++ task-3/Equation.java
@@ -6,18 +6,15 @@ public class Equation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int X = scanner.nextInt();
-        int N = scanner.nextInt();
-        int result = calculateEquationResult(X, N);
-        System.out.println(result);
-    }
-    static int calculateEquationResult(int X, int N) {
-        int result = 0;
-
-        for (int i = 0; i <= N; i += 2) {
-            result += Math.pow(X, i);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long E= 0;
+         
+        for (long i = 2; i <= b; i += 2) {
+            E += Math.pow(a, i);
         }
-
-        return result;
+        
+        System.out.println(E);
     }
 }
+
