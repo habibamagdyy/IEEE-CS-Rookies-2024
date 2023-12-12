ca947d9cde593462120f930e484470ce70814ead
habibamagdyy <115741344+habibamagdyy@users.noreply.github.com>
2023-12-12 22:30:54 +0200

    Update Print.java

--- task-3/Print.java
+++ task-3/Print.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 public class Print {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+Scanner scanner = new Scanner(System.in);
 
-        printNumbers(N);
+        int x = scanner.nextInt();
+        printNumbers(x);
+        scanner.close();
     }
 
-    static void printNumbers(int N) {
-        for (int i = 1; i <= N; i++) {
-            System.out.print(i + " ");
+    private static void printNumbers(int x) {
+        for (int i = 1; i <= x; i++) {
+            System.out.print(i);
+            if (i < x) {
+                System.out.print(" ");
+            }
         }
+        System.out.println();
     }
 }
